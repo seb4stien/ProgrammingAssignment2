b370aa65709838cd48c28936c0b114c485bbac08
seb4stien <seb4stien@georget.name>
2014-10-18 17:03:59 +0200

    my homeworks

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+# Functions to create a special object that stores 
+# a numeric matrix and cache's its inverse.
+#
 
-## Write a short comment describing this function
 
+## The 'CacheMatrix' constructor
+# returns an object with the following methods
+#  set : sets the content of the matrix
+#  get : returns the content of the matrix
+#  setInverse : sets the inverse of the matrix
+#  getInverse : gets the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
 
+  # variable to store the cached inverse
+  i <- NULL
+  
+  # to set the matrix data
+  set <- function (m) {
+    x <<- m
+    
+    # invalidate the cache if the matrix is modified
+    i <<- NULL
+  }
+  
+  # to get the matrix data
+  get <- function () x
+  
+  # to set the inverse of the matrix
+  setInverse <- function (inverse) i <<- inverse
+  
+  # to get the inverse of the matrix
+  getInverse <- function () i
+  
+  # return the CacheMatrix object with its methods
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The function to calculate the inverse of the matrix
+# If the inverse has never been calculated
+#    it calculates it and caches it
+# If it has aleady been calculated
+#    it returns the result from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        i <- m$getInverse()
+        
+        if (!is.null(i)) {
+          # the cache exists
+          message("getting cached data")
+        } else {
+          # the cache doesn't exist, so we get the data
+          data <- m$get()
+          
+          # calcultate the inverse
+          i <- solve(data)
+          
+          # cache it 
+          m$setInverse(i)
+        }
+        
+        # return the inverse
+        i
 }
